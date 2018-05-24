1b0b349d21ebffeea00e19dd41ef51c498a920a7
sudhagara2 <sudhagara2@gmail.com>
2018-05-24 12:58:01 +0530

    Query changed

--- app/models/distributor_allocation.rb
+++ app/models/distributor_allocation.rb
@@ -1,6 +1,6 @@
-class DistributorAllocation < ApplicationRecord
-  belongs_to :distributor
+class DistributorAllocation < ApplicationRecord  
   belongs_to :country
   belongs_to :province
   belongs_to :city
+  serialize :distributor_id, Array
 end
